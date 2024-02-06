863d39e9b827eca5dddf04368a944a197a625a4a
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2024-02-06 11:18:17 -0300

    stage 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-[2000px]">
-      {/* TOP CONTENT */}
+    <main className="flex flex-col items-center min-h-[3500px]">
+      {/* STAGE 1 */}
       <div className="flex justify-between w-full mt-20">
         <Image src="/mark-1.svg" alt="Stage 1" width={190} height={295} />
         <div className="flex flex-col max-w-[505px]">
@@ -35,6 +35,89 @@ export default function Home() {
           </div>
         </div>
         <Image src="/mascout.svg" alt="Mascout" width={592} height={775} />
+      </div>
+      {/* STAGE 2 */}
+      <div className="flex justify-between w-full">
+        <Image
+          src="/rocket-in-space.svg"
+          alt="Rocket"
+          width={466.14}
+          height={935}
+        />
+        <div className="flex flex-col max-w-[716px]">
+          {/* TITLE */}
+          <div className="flex flex-col text-blue-love leading-[60px] mt-14 ml-[-200px]">
+            <p className="font-bold text-[96px] tracking-[-2px]">L2 + VE?</p>
+            <p className="font-bold text-[40px]">WHAT DOES THIS EVEN MEAN?</p>
+          </div>
+          {/* BOXES */}
+          <div className="flex flex-col gap-14 font-medium text-[20px] text-blue-love mt-[40px]">
+            <div className="py-6 px-12 shadow-lg rounded-[20px] bg-white text-center ml-[-200px] max-w-[780px]">
+              Cupid Inu is in <span className="font-bold">$L2VE</span> with{" "}
+              <span className="font-bold">L2 Blockchains</span>, but even more
+              he's in <span className="font-bold">$L2VE</span> with passive
+              income! By accumulating{" "}
+              <span className="font-bold">ve3,3 voting power</span> through a
+              tax, he wants to create an everlasting APR for $L2VE, because{" "}
+              <span className="font-bold">$L2VE never ends</span>!
+            </div>
+            <div className="flex items-center py-3 px-12 shadow-lg rounded-[20px] bg-white text-center ml-[-100px] max-w-[780px] gap-10">
+              <div className="flex items-center justify-center">
+                <Image
+                  src="/jerrycan.svg"
+                  alt="jerrycan"
+                  width={135}
+                  height={135}
+                />
+
+                <div className="flex flex-col">
+                  <span className="font-bold text-[48px] tracking-[-4px] text-center">
+                    STAGE I
+                  </span>
+                  <Image
+                    src="/uniswap.svg"
+                    alt="uniswap"
+                    width={225}
+                    height={56}
+                    className="mt-[-15px]"
+                  />
+                </div>
+              </div>
+              <div className="min-w-1 h-full bg-blue-love font-bold text-[16px]"></div>
+              <div>Deploy Liquidity on UniSwap v3.</div>
+            </div>
+
+            <div className="flex items-center py-3 px-12 shadow-lg rounded-[20px] bg-white text-center ml-[-180px] max-w-[780px] gap-16">
+              <div className="flex items-center justify-center w-full">
+                <Image
+                  src="/jerrycan.svg"
+                  alt="jerrycan"
+                  width={135}
+                  height={135}
+                />
+
+                <div className="flex flex-col">
+                  <span className="font-bold text-[48px] tracking-[-4px] text-center w-max">
+                    STAGE II
+                  </span>
+                  <Image
+                    src="/aerodrome.svg"
+                    alt="uniswap"
+                    width={230}
+                    height={33}
+                    className="mt-[-5px]"
+                  />
+                </div>
+              </div>
+              <div className="min-w-1 h-full bg-blue-love font-bold text-[16px]"></div>
+              <div>
+                Deploy Liquidity on Aerodome. Vote with veAERO on $L2VE pair.
+                Good APR. Forever.
+              </div>
+            </div>
+          </div>
+        </div>
+        <Image src="/mark-2.svg" alt="Stage 1" width={202} height={295} />
       </div>
     </main>
   );
