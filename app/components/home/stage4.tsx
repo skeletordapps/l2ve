9ad00ceb9c61512de8418ebf26318783bc89ca08
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2024-02-12 11:27:09 -0300

    fix rockets space

--- app/components/home/stage4.tsx
+++ app/components/home/stage4.tsx
@@ -8,7 +8,7 @@ export default function Stage4() {
       className="flex flex-col xl:flex-row justify-between w-full mt-12 sm:mt-24 xl:mt-32"
     >
       <div className="hidden xl:block " />
-      <div className="flex flex-col w-full px-6 md:px-12 xl:px-20 2xl:px-0">
+      <div className="flex flex-col w-full px-6 md:px-12 xl:px-20">
         {/* TITLE */}
         <div className="flex flex-col text-white leading-[50px] xl:leading-[70px] font-bold">
           <p className="text-[56px] sm:text-[66px] 2xl:text-[96px]">
