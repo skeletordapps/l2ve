adeb6533bf48c86d812992f702db52e6a437b907
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2024-03-03 22:18:32 -0300

    check mark on first 2 stages

--- app/components/home/stage2.tsx
+++ app/components/home/stage2.tsx
@@ -32,7 +32,7 @@ export default function Stage2() {
               tax, he wants to create an everlasting APR for $L2VE, because{" "}
               <span className="font-bold">$L2VE never ends</span>!
             </div>
-            <div className="flex items-center py-3 px-12 shadow-lg rounded-[20px] bg-white dark:bg-[#11151E] text-center ml-[-100px] max-w-[780px] gap-10">
+            <div className="flex items-center py-3 px-12 shadow-lg rounded-[20px] bg-white dark:bg-[#11151E] text-center ml-[-100px] max-w-[780px] gap-10 relative">
               <div className="flex items-center justify-center">
                 <Image
                   src="/jerrycan.svg"
@@ -57,9 +57,25 @@ export default function Stage2() {
               </div>
               <div className="min-w-1 h-full bg-blue-love dark:bg-dark-love font-bold text-[16px]"></div>
               <div>Deploy Liquidity on UniSwap v3.</div>
+
+              <div className="absolute top-2 right-2 w-8 h-8 text-green-400">
+                <svg
+                  data-slot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                  ></path>
+                </svg>
+              </div>
             </div>
 
-            <div className="flex items-center py-3 px-12 shadow-lg rounded-[20px] bg-white dark:bg-[#11151E] text-center ml-[-180px] max-w-[780px] gap-16">
+            <div className="flex items-center py-3 px-12 shadow-lg rounded-[20px] bg-white dark:bg-[#11151E] text-center ml-[-180px] max-w-[780px] gap-16 relative">
               <div className="flex items-center justify-center w-full">
                 <Image
                   src="/jerrycan.svg"
@@ -86,6 +102,22 @@ export default function Stage2() {
               <div>
                 Deploy Liquidity on Aerodome. Vote with veAERO on $L2VE pair.
                 Good APR. Forever.
+              </div>
+
+              <div className="absolute top-2 right-2 w-8 h-8 text-green-400">
+                <svg
+                  data-slot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                  ></path>
+                </svg>
               </div>
             </div>
           </div>
@@ -132,7 +164,7 @@ export default function Stage2() {
               tax, he wants to create an everlasting APR for $L2VE, because{" "}
               <span className="font-bold">$L2VE never ends</span>!
             </div>
-            <div className="flex flex-col sm:flex-row items-center py-3 sm:px-8 shadow-lg rounded-[20px] bg-white dark:bg-[#11151E] sm:text-center gap-4 sm:gap-10">
+            <div className="flex flex-col sm:flex-row items-center py-3 sm:px-8 shadow-lg rounded-[20px] bg-white dark:bg-[#11151E] sm:text-center gap-4 sm:gap-10 relative">
               <div className="flex items-center justify-center">
                 <Image
                   src="/jerrycan.svg"
@@ -160,9 +192,24 @@ export default function Stage2() {
               <div className="px-10 sm:px-0">
                 Deploy Liquidity on UniSwap v3.
               </div>
+              <div className="absolute top-2 right-2 w-8 h-8 text-green-400">
+                <svg
+                  data-slot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                  ></path>
+                </svg>
+              </div>
             </div>
 
-            <div className="flex flex-col sm:flex-row items-center py-3 sm:px-8 shadow-lg rounded-[20px] bg-white dark:bg-[#11151E] sm:text-center gap-4 sm:gap-10">
+            <div className="flex flex-col sm:flex-row items-center py-3 sm:px-8 shadow-lg rounded-[20px] bg-white dark:bg-[#11151E] sm:text-center gap-4 sm:gap-10 relative">
               <div className="flex items-center justify-center">
                 <Image
                   src="/jerrycan.svg"
@@ -190,6 +237,21 @@ export default function Stage2() {
               <div className="px-10 sm:px-0">
                 Deploy Liquidity on Aerodome. Vote with veAERO on $L2VE pair.
                 Good APR. Forever.
+              </div>
+              <div className="absolute top-2 right-2 w-8 h-8 text-green-400">
+                <svg
+                  data-slot="icon"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                  ></path>
+                </svg>
               </div>
             </div>
           </div>
