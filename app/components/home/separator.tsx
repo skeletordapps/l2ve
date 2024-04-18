63c1b3a9b848e86d998f29e83a48b3e9f58834ad
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2024-04-18 19:25:29 -0300

    wallet connect text

--- app/components/home/separator.tsx
+++
@@ -1,7 +0,0 @@
-export default function Separator() {
-  return (
-    <div className="flex items-center justify-center w-full h-[127px] px-[50px] xl:px-[100px] sm:mt-16">
-      <div className="flex items-center justify-center w-full h-[127px] bg-separator bg-cover bg-no-repeat bg-center" />
-    </div>
-  );
-}
